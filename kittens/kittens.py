fb32b8f814373ff15345ad839f08d68931ec3a36
brittanymcgarr <brittanymcgarr@nevada.unr.edu>
2018-09-10 21:43:41 -0700

    Creating boilerplate

---
+++ kittens/kittens.py
@@ -0,0 +1,18 @@
+################################################################################
+## Kittens in a Blender Demo                                                  ##
+## The only blending game for kittens.                                        ##
+################################################################################
+
+from flask import Flask, render_template
+
+# initialize the Flask app
+app = Flask(__name__)
+
+# create the home route
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
