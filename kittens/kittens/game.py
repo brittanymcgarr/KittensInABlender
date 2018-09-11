d87cdd901c407e4d2c227d25086bd82812bfdb54
brittanymcgarr <brittanymcgarr@nevada.unr.edu>
2018-09-10 21:49:49 -0700

    Initial commit

---
+++ kittens/kittens/game.py
@@ -0,0 +1,5 @@
+################################################################################
+## Kittens in a Blender Game Logic                                            ##
+## Creates and manages the game layout, cards, players, and win conditions    ##
+################################################################################
+
