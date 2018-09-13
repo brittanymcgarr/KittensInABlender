6cd968c9b217ad740d584165f56ecef72e97c788
brittanymcgarr <brittanymcgarr@nevada.unr.edu>
2018-09-12 20:25:06 -0700

    Adding base game

--- kittens/kittens/game.py
+++ kittens/kittens/game.py
@@ -3,3 +3,20 @@
 ## Creates and manages the game layout, cards, players, and win conditions    ##
 ################################################################################
 
+
+CARD_TYPE = ['kitten', 'blend', 'action']
+COLORS = ['blue', 'pink', 'green', 'yellow', 'purple']
+
+
+class Player:
+    def __init__(self, name, id, color):
+        self.name = name
+        self.id = id
+        self.color = color
+
+
+class Card:
+    def __init__(self, type, title, owner_id=None):
+        self.type = type
+        self.title = title
+        self.owner_id = owner_id
